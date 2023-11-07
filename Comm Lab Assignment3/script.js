edc692a11ba794a957a04887b2f0639f180d72a7
Fady John <63600279+FadyJohn10@users.noreply.github.com>
2023-11-07 17:01:04 +0400

    update

--- Comm Lab Assignment3/script.js
+++ Comm Lab Assignment3/script.js
@@ -1,12 +1,21 @@
+
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById('intro_audio').play();
+});
+
+document.getElementById('intro_audio').play();
+
 function dIntro(){
     document.getElementById("readybtn").remove();
+    document.getElementById("vid").style.filter = "brightness(1)";
     document.getElementById('vid').play();
     setTimeout(function(){
-        
         $(document).ready(function() {
-                $("#vid").fadeOut(1000); // 1000 milliseconds (1 second) duration
+                $("#vid").fadeOut(1000);
+                $("#main").fadeIn(500);
         });
-
-        document.getElementById("main").style.display = "block";
+        setTimeout(function(){
+            document.getElementById('audio').play();
+        }, 1000);
     }, 4000);
 }
