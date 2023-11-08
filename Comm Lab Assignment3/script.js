5e93aa2c5ac1cd158bab99a5fab912fcdf7e3830
Fady John <63600279+FadyJohn10@users.noreply.github.com>
2023-11-08 22:20:02 +0400

    1

--- Comm Lab Assignment3/script.js
+++ Comm Lab Assignment3/script.js
@@ -1,21 +1,51 @@
-
+// This event listener ensures that the code runs when the document is fully loaded.
 document.addEventListener("DOMContentLoaded", function() {
     document.getElementById('intro_audio').play();
 });
 
-document.getElementById('intro_audio').play();
+// Function for scrolling the page automatically
+function scrollPage() {
 
-function dIntro(){
+    // Scroll the window by 0.5 pixels vertically.
+    window.scrollBy(0, 0.5);
+    if (window.scrollY < document.body.clientHeight - window.innerHeight) {
+        requestAnimationFrame(scrollPage);
+    }
+}
+
+// Function for initiating the intro sequence
+function dIntro(country) {
+    // Remove the 'readybtn' element from the page.
     document.getElementById("readybtn").remove();
+
+    // Adjust the brightness of the 'vid' element to 100%
     document.getElementById("vid").style.filter = "brightness(1)";
+
+    // Play the intro video
     document.getElementById('vid').play();
-    setTimeout(function(){
+
+    // Set a timeout to control the timing of subsequent actions.
+    setTimeout(function() {
+        // Ensure the document is fully ready.
         $(document).ready(function() {
-                $("#vid").fadeOut(1000);
-                $("#main").fadeIn(500);
+            $("#vid").fadeOut(500);
+            $("#main").fadeIn(500);
         });
-        setTimeout(function(){
+
+        // Set a timeout to control the timing of the main audio and scrolling.
+        setTimeout(function() {
             document.getElementById('audio').play();
+            if(country == 'pakistan'){
+                changeImage();
+            }
+            // Start scrolling the page automatically.
+            scrollPage();
         }, 1000);
     }, 4000);
+}
+
+function changeImage(){
+    setTimeout(function(){
+        document.querySelector(".pakistan_parllax").style.backgroundImage = "url('./images/pakistan2.png')";
+    }, 30000)
 }
