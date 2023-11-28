c2c880a454719a30328b2239a54f0a063567a03e
Fady John <63600279+FadyJohn10@users.noreply.github.com>
2023-11-29 02:01:01 +0400

    ..

--- Comm Lab Assignment4/script.js
+++ Comm Lab Assignment4/script.js
@@ -1,15 +1,2 @@
 let video = document.querySelector(".video");
 video.height = video.clientWidth * 0.56;
-let vidDuration;
-video.addEventListener('loadedmetadata', function() {
-  vidDuration = video.duration;
-  
-});
-
-function checkTime(){
-  if (video.currentTime >= vidDuration) {
-    // display buttons the specific second is reached
-    document.getElementById("buttons").classList.remove("d-none");
-  }
-}
-video.addEventListener('timeupdate', checkTime);
